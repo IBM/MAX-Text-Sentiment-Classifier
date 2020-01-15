3b4cd4cf24e3d889c47efcab5009f823ad589d58
Jhuo IH <41447049+autoih@users.noreply.github.com>
2020-01-15 10:51:28 -0800

    Removal of __future__ boilerplate

--- training/training_code/bert/modeling.py
+++ training/training_code/bert/modeling.py
@@ -14,10 +14,6 @@
 # limitations under the License.
 """The main BERT model and related functions."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import collections
 import copy
 import json
