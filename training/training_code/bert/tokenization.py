dd479e61e545fe541c723d2e6e9d3fbbefaedc0e
Jhuo IH <41447049+autoih@users.noreply.github.com>
2020-01-15 10:55:33 -0800

    Removal of __future__ boilerplate

--- training/training_code/bert/tokenization.py
+++ training/training_code/bert/tokenization.py
@@ -14,10 +14,6 @@
 # limitations under the License.
 """Tokenization classes."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import collections
 import re
 import unicodedata
