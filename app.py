5721e3ad3f862941b509c009ae0cc2402f45e390
SSaishruthi <saishruthi.tn@ibm.com>
2021-04-06 21:56:12 -0700

    update maxfw version

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ from config import API_TITLE, API_DESC, API_VERSION
 max = MAXApp(API_TITLE, API_DESC, API_VERSION)
 max.add_api(ModelMetadataAPI, '/metadata')
 max.add_api(ModelPredictAPI, '/predict')
-max.run()
+max.run(port=8080)
 #app.run(debug=True, host='0.0.0.0', port=8080, use_reloader=True)
